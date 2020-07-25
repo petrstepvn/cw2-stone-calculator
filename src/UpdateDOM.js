8e823cb07be4e2e721363fe41a44209bc9130d78
petrstepvn <petrstepvn@gmail.com>
2020-07-25 17:40:16 +0200

    added current lvl req xp

--- src/UpdateDOM.js
+++ src/UpdateDOM.js
@@ -1,40 +1,49 @@
 import Data from './data.json';
 import { thousandSeparator } from './utils';
 import {
-	inputElementsDOM,
-	resultElementsDOM,
-	inputData,
-	resultData,
+  inputElementsDOM,
+  resultElementsDOM,
+  inputData,
+  resultData,
 } from './index';
 
 export const updateInputElements = () => {
-	const refinementLevelSliderDOM = inputElementsDOM.find(
-		(el) => el.id === 'refinementLevelSlider'
-	);
-	inputData.stoneGrade
-		? (refinementLevelSliderDOM.disabled = true)
-		: (refinementLevelSliderDOM.disabled = false);
+  const { InfectedExpPerLevel } = Data;
+  const { stoneGrade, refinementLevel, infectedLevel } = inputData;
 
-	const refinementLevels = inputElementsDOM.filter((el) =>
-		/refinementLevel/i.test(el.id)
-	);
-	const infectedLevels = inputElementsDOM.filter((el) =>
-		/infectedLevel/i.test(el.id)
-	);
-	refinementLevels.forEach((el) => (el.value = inputData.refinementLevel));
-	infectedLevels.forEach((el) => (el.value = inputData.infectedLevel));
+  const refinementLevelSliderDOM = inputElementsDOM.find(
+    (el) => el.id === 'refinementLevelSlider'
+  );
+  stoneGrade
+    ? (refinementLevelSliderDOM.disabled = true)
+    : (refinementLevelSliderDOM.disabled = false);
 
-	const infectedLevelXpDOM = document.getElementById('infectedLevelXp');
-	const { InfectedExpPerLevel } = Data;
-	const { infectedLevel } = inputData;
-	const requiredXp = InfectedExpPerLevel.slice(0, infectedLevel + 1).reduce(
-		(acc, curr) => acc + curr
-	);
-	infectedLevelXpDOM.textContent = thousandSeparator(requiredXp);
+  const refinementLevels = inputElementsDOM.filter((el) =>
+    /refinementLevel/i.test(el.id)
+  );
+  const infectedLevels = inputElementsDOM.filter((el) =>
+    /infectedLevel/i.test(el.id)
+  );
+  refinementLevels.forEach((el) => (el.value = refinementLevel));
+  infectedLevels.forEach((el) => (el.value = infectedLevel));
+
+  const infectedLevelSmall = document.querySelectorAll('.infectedLevelSmall');
+  const infectedLevelXpDOM = document.getElementById('infectedLevelXp');
+  const infectedLevelXpTotalDOM = document.getElementById(
+    'infectedLevelXpTotal'
+  );
+  const requiredXp = InfectedExpPerLevel[infectedLevel];
+  const requiredXpTotal = InfectedExpPerLevel.slice(
+    0,
+    infectedLevel + 1
+  ).reduce((acc, curr) => acc + curr);
+  infectedLevelXpDOM.textContent = thousandSeparator(requiredXp);
+  infectedLevelXpTotalDOM.textContent = thousandSeparator(requiredXpTotal);
+  infectedLevelSmall.forEach((el) => (el.textContent = infectedLevel + 1));
 };
 
 export const updateResultsDOM = () => {
-	resultElementsDOM.forEach((element) => {
-		element.textContent = thousandSeparator(resultData[element.id]);
-	});
+  resultElementsDOM.forEach((element) => {
+    element.textContent = thousandSeparator(resultData[element.id]);
+  });
 };
