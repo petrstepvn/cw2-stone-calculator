297d16eba413c11bdc5151722c79736472d3e321
petrstepvn <petrstepvn@gmail.com>
2020-07-25 23:37:29 +0200

    added X-30 req xp

--- src/UpdateDOM.js
+++ src/UpdateDOM.js
@@ -28,30 +28,39 @@ export const updateInputElements = () => {
   infectedLevels.forEach((el) => (el.value = infectedLevel));
 
   const infectedLevelSmall = document.querySelectorAll('.infectedLevelSmall');
-  const infectedLevelXpDOM = document.getElementById('infectedLevelXp');
-  const infectedLevelXpFragmentDOM = document.getElementById(
-    'infectedLevelXpFragment'
+
+  const infLvl = [
+    'infectedLevelXp',
+    'infectedLevelXpTotal',
+    'infectedLevelXpTotalEnd',
+  ];
+  const infLvlFrag = [
+    'infectedLevelXpFragment',
+    'infectedLevelXpFragmentTotal',
+    'infectedLevelXpFragmentTotalEnd',
+  ];
+  const infLvlDOM = infLvl.map((el) => document.getElementById(el));
+  const infLvlFragDOM = infLvlFrag.map((el) => document.getElementById(el));
+
+  const xp = InfectedExpPerLevel[infectedLevel];
+  const xpTotal = InfectedExpPerLevel.slice(0, infectedLevel + 1).reduce(
+    (acc, curr) => acc + curr
   );
-  const infectedLevelXpTotalFragmentDOM = document.getElementById(
-    'infectedLevelXpTotalFragment'
+  const xpTotalEnd = InfectedExpPerLevel.slice(infectedLevel, 31).reduce(
+    (acc, curr) => acc + curr
   );
-  const infectedLevelXpTotalDOM = document.getElementById(
-    'infectedLevelXpTotal'
-  );
-  const requiredXp = InfectedExpPerLevel[infectedLevel];
-  const requiredXpTotal = InfectedExpPerLevel.slice(
-    0,
-    infectedLevel + 1
-  ).reduce((acc, curr) => acc + curr);
-  const infFragment = Math.ceil(requiredXp / 2000);
-  const infFragmentTotal = Math.ceil(requiredXpTotal / 2000);
+
+  const xpFrag = Math.ceil(xp / 2000);
+  const xpFragTotal = Math.ceil(xpTotal / 2000);
+  const xpFragTotalEnd = Math.ceil(xpTotalEnd / 2000);
+
   infectedLevelSmall.forEach((el) => (el.textContent = infectedLevel));
-  infectedLevelXpDOM.textContent = thousandSeparator(requiredXp);
-  infectedLevelXpTotalDOM.textContent = thousandSeparator(requiredXpTotal);
-  infectedLevelXpFragmentDOM.textContent = thousandSeparator(infFragment);
-  infectedLevelXpTotalFragmentDOM.textContent = thousandSeparator(
-    infFragmentTotal
-  );
+  infLvlDOM[0].textContent = thousandSeparator(xp);
+  infLvlDOM[1].textContent = thousandSeparator(xpTotal);
+  infLvlDOM[2].textContent = thousandSeparator(xpTotalEnd);
+  infLvlFragDOM[0].textContent = thousandSeparator(xpFrag);
+  infLvlFragDOM[1].textContent = thousandSeparator(xpFragTotal);
+  infLvlFragDOM[2].textContent = thousandSeparator(xpFragTotalEnd);
 };
 
 export const updateResultsDOM = () => {
